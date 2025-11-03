56809f71477363f46af831a24d977c4edcc98c70
Jack <jkotheimer9@gmail.com>
2025-11-03 11:03:42 -0600

    Updated schema and user sql files to use templates for all variables that can be set during configuration

--- sql/app-schema.sql
+++ sql/app-schema.sql
@@ -1,13 +1,13 @@
-DELIMITER //
-
-CREATE DATABASE IF NOT EXISTS typescript_server_starter_kit;
-USE typescript_server_starter_kit;
+CREATE DATABASE IF NOT EXISTS {tsk_database};
+USE {tsk_database};
+bullshit
 
 DROP PROCEDURE IF EXISTS initialize_db;
 
-CREATE PROCEDURE initialize_db ()
-BEGIN
+DELIMITER //
 
+CREATE PROCEDURE initialize_db()
+BEGIN
     DECLARE EXIT HANDLER FOR SQLEXCEPTION
     BEGIN
         ROLLBACK;
@@ -15,35 +15,13 @@ BEGIN
 
     START TRANSACTION;
 
-    CREATE TABLE Organization (
-        Id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-        Name NVARCHAR(255) NOT NULL,
-        Email NVARCHAR(255) NOT NULL,
-        PRIMARY KEY (Id)
-    ) CHARACTER SET utf8mb4;
-
-    CREATE TABLE Role (
-        Id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-        Name NVARCHAR(255) NOT NULL,
-        Label NVARCHAR(255) NOT NULL,
-        Type CHAR(1) NOT NULL,
-        PRIMARY KEY (Id)
-    ) CHARACTER SET utf8mb4;
-
-    CREATE UNIQUE INDEX RoleName ON Role(Name);
-
-    INSERT INTO Role (Name, Label, Type) VALUES
-        ('OrganizationAdministrator', 'Organization Administrator', 'd'),
-        ('RegisteredUser', 'Registered User', 'd'),
-        ('GuestUser', 'Guest User', 'd');
-
     CREATE TABLE User (
         Id INT UNSIGNED NOT NULL AUTO_INCREMENT,
         Email NVARCHAR(255) NOT NULL,
         Phone NVARCHAR(32),
         FirstName NVARCHAR(255),
         LastName NVARCHAR(255),
-        Password NVARCHAR(8191) NOT NULL,
+        CurrentPasswordId INT UNSIGNED NOT NULL,
         EmailVerified BOOLEAN DEFAULT FALSE,
         IsActive BOOLEAN DEFAULT TRUE,
         CreatedDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
@@ -54,31 +32,81 @@ BEGIN
 
     CREATE UNIQUE INDEX UserEmail ON User(email);
 
+    CREATE TABLE UserCredentialType (
+        Value NCHAR(32) NOT NULL,
+        PRIMARY KEY (Value)
+    ) CHARACTER SET utf8mb4;
+
+    INSERT INTO UserCredentialType (Value) VALUES ('password'), ('jwt');
+
+
+    CREATE TABLE UserCredential (
+        Id INT UNSIGNED AUTO_INCREMENT,
+        Value VARCHAR(4096) NOT NULL,
+        UserId INT UNSIGNED NOT NULL,
+        Type NCHAR(32) NOT NULL,
+        IsActive BOOLEAN DEFAULT TRUE,
+        ExpirationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+        CONSTRAINT CredentialUser
+            FOREIGN KEY (UserId) REFERENCES User(Id)
+            ON DELETE CASCADE
+            ON UPDATE RESTRICT,
+        CONSTRAINT UserCredentialType
+            FOREIGN KEY (Type) REFERENCES UserCredentialType(Value)
+            ON DELETE RESTRICT
+            ON UPDATE RESTRICT,
+        PRIMARY KEY (Id)
+    ) CHARACTER SET utf8mb4;
+
+    ALTER TABLE User
+        ADD CONSTRAINT UserCurrentPassword 
+        FOREIGN KEY (CurrentPasswordId) REFERENCES UserCredential(Id);
+
+    CREATE TABLE Organization (
+        Id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        Name NVARCHAR(255) NOT NULL,
+        Email NVARCHAR(255) NOT NULL,
+        PRIMARY KEY (Id)
+    ) CHARACTER SET utf8mb4;
+
+    CREATE TABLE OrganizationRole (
+        Id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        Name NVARCHAR(255) NOT NULL,
+        Label NVARCHAR(255) NOT NULL,
+        OrganizationId INT UNSIGNED NOT NULL,
+        PRIMARY KEY (Id)
+    ) CHARACTER SET utf8mb4;
+
+    CREATE UNIQUE INDEX RoleNameWithinOrg ON Role(Name, OrganizationId);
+
     CREATE TABLE OrganizationUser (
-        Organization INT UNSIGNED NOT NULL,
-        User INT UNSIGNED NOT NULL,
-        Role INT UNSIGNED NOT NULL,
+        OrganizationId INT UNSIGNED NOT NULL,
+        UserId INT UNSIGNED NOT NULL,
+        RoleId INT UNSIGNED NOT NULL,
         CONSTRAINT OrganizationUserOrganization
-            FOREIGN KEY (Organization) REFERENCES Organization(Id)
+            FOREIGN KEY (OrganizationId) REFERENCES Organization(Id)
             ON DELETE CASCADE
             ON UPDATE RESTRICT,
         CONSTRAINT OrganizationUserUser
-            FOREIGN KEY (User) REFERENCES User(Id)
+            FOREIGN KEY (UserId) REFERENCES User(Id)
             ON DELETE CASCADE
             ON UPDATE RESTRICT,
         CONSTRAINT OrganizationUserRole
-            FOREIGN KEY (Role) REFERENCES Role(Id)
+            FOREIGN KEY (RoleId) REFERENCES OrganizationRole(Id)
             ON DELETE CASCADE
             ON UPDATE RESTRICT,
-        PRIMARY KEY (Organization, User)
+        PRIMARY KEY (OrganizationId, UserId)
     ) CHARACTER SET utf8mb4;
 
     CREATE TABLE Post (
         Id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-        Author INT UNSIGNED NOT NULL,
+        AuthorId INT UNSIGNED NOT NULL,
         Title NVARCHAR(255),
         Body NVARCHAR(8191),
-        FOREIGN KEY (Author) REFERENCES User(Id),
+        CONSTRAINT PostAuthor
+            FOREIGN KEY (AuthorId) REFERENCES User(Id)
+            ON DELETE CASCADE
+            ON UPDATE RESTRICT,
         PRIMARY KEY (Id)
     ) CHARACTER SET utf8mb4;
 
--- sql/create-user.sql
+++ sql/create-user.sql
@@ -1,4 +1,25 @@
-DROP USER IF EXISTS 'tssk_user'@'localhost';
-CREATE USER IF NOT EXISTS 'tssk_user'@'localhost' IDENTIFIED BY 'tssk_password';
-GRANT SELECT, INSERT, UPDATE, DELETE ON typescript_server_starter_kit.* TO 'tssk_user'@'localhost';
-FLUSH PRIVILEGES;
+USE tssk;
+
+DROP PROCEDURE IF EXISTS create_tsk_user;
+
+DELIMITER //
+
+CREATE PROCEDURE create_tsk_user ()
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        ROLLBACK;
+    END;
+
+    START TRANSACTION;
+
+    DROP USER IF EXISTS '{tsk_user}'@'{tsk_host}';
+    CREATE USER IF NOT EXISTS '{tsk_user}'@'{tsk_host}' IDENTIFIED BY '{tsk_password}';
+    GRANT SELECT, INSERT, UPDATE, DELETE ON {tsk_database}.* TO '{tsk_user}'@'{tsk_host}';
+    FLUSH PRIVILEGES;
+    COMMIT;
+END //
+
+DELIMITER ;
+
+CALL create_tsk_user();
