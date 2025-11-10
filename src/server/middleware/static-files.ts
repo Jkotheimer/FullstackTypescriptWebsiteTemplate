59e8154718c94f26ea6ad9578161be210dfc1976
Jack <jkotheimer9@gmail.com>
2025-11-09 23:43:53 -0600

    Fixed compilation error by adding __dirname definition

--- src/server/middleware/static-files.ts
+++ src/server/middleware/static-files.ts
@@ -1,5 +1,9 @@
 import Express from 'express';
 import path from 'path';
+import url from 'url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default Express.static(path.resolve(__dirname, '/public'), {
     immutable: true,
